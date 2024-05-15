e619bc0ab3eb246060dcc0826ba4234ec4196778
vahid <vahid.hossaini@gmail.com>
2024-05-15 11:22:47 +0330

    cloud config added

---
+++ src/models/cloudConfig.ts
@@ -0,0 +1,18 @@
+export enum CloudType
+{
+    UniqueInCloud=1,
+    UniqueInServer=2,
+    MultiProcess=3
+}
+export default class CloudConfig
+{
+    group:string;
+    type:CloudType=CloudType.MultiProcess;
+    constructor(data:{
+        group:string;
+        type?:CloudType; 
+    })
+    {
+        Object.assign(this,data);
+    }
+}
--- src/models/moduleConfig.ts
+++ src/models/moduleConfig.ts
@@ -1,13 +1,16 @@
+import CloudConfig from "./cloudConfig";
 import PackageIndex from "./packageIndex";
 
 export default abstract class ModuleConfig
 {
     id:string='' 
     inMainThread:boolean;
+    cloud:CloudConfig;
     public constructor(
         fields?: {
           id?: string,  
           inMainThread?:boolean
+          cloud?:CloudConfig
         }) {
         if (fields) Object.assign(this, fields);
         if(!this.id)this.id=Math.random().toString();
